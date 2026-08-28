5c9ea083f4ef8dc36def3194a50fc265497de8a5
Giovanni Ferri <giovanni@syscode.uk>
2026-08-28 22:12:04 +0100

    fix(e2e): bound helper command execution to fail fast on wedged apiserver
    Run had no timeout, so a single wedged kubectl inside an Eventually
    froze the whole suite past the global go test wall (25m). Now Run uses
    a 60s default via RunWithTimeout and long operations (helm install
    --wait, kind load, cert-manager wait) use explicit longer timeouts.

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -92,7 +92,7 @@ var _ = BeforeSuite(func() {
 	By("installing imp-crds chart")
 	crdsCmd := exec.Command("helm", "install", helmCRDRelease, "charts/imp-crds",
 		"--namespace", namespace, "--wait", "--create-namespace")
-	_, err := utils.Run(crdsCmd)
+	_, err := utils.RunWithTimeout(6*time.Minute, crdsCmd)
 	Expect(err).NotTo(HaveOccurred(), "helm install imp-crds failed")
 
 	By("installing imp chart")
@@ -142,7 +142,7 @@ var _ = BeforeSuite(func() {
 	impArgs = append(impArgs, "--wait", "--timeout", "10m")
 
 	impCmd := exec.Command("helm", impArgs...)
-	_, err = utils.Run(impCmd)
+	_, err = utils.RunWithTimeout(12*time.Minute, impCmd)
 	Expect(err).NotTo(HaveOccurred(), "helm install imp failed")
 
 	By("installing imp-sandbox chart")
@@ -153,7 +153,7 @@ var _ = BeforeSuite(func() {
 		"--set", "sandbox.image.repository="+sandboxImageRepo,
 		"--set", "sandbox.image.tag="+sandboxImageTag,
 		"--wait", "--timeout", "5m")
-	_, err = utils.Run(sandboxCmd)
+	_, err = utils.RunWithTimeout(6*time.Minute, sandboxCmd)
 	Expect(err).NotTo(HaveOccurred(), "helm install imp-sandbox failed")
 
 	By("disabling scheduling reserve for tiny kind cluster")
